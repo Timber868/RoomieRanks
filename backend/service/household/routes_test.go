86f17ca93b10b83eceb30ad18768f7d34581e775
Tim.roma <Timroma6@gmail.com>
2025-01-25 14:57:45 -0500

    Added the household and chore types

---
+++ backend/service/household/routes_test.go
@@ -0,0 +1,9 @@
+package household
+
+import "testing"
+
+func TestUserServiceHandlers(t *testing.T) {
+	t.Run("Invalid payload", func(t *testing.T) {
+		t.Skip("Not implemented")
+	})
+}
--- backend/types/types.go
+++ backend/types/types.go
@@ -2,6 +2,8 @@ package types
 
 import "time"
 
+// -- User types
+
 //This is the type i am going to be using to read json data sent and parse payload
 type RegisterUserPayload struct {
 	FirstName string `json:"firstName" validate:"required"`
@@ -11,12 +13,13 @@ type RegisterUserPayload struct {
 }
 
 type User struct {
-	ID        int       `json:"ID"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
-	CreatedAt time.Time `json:"createdAT"`
+	ID          int       `json:"ID"`
+	FirstName   string    `json:"firstName"`
+	LastName    string    `json:"lastName"`
+	Email       string    `json:"email"`
+	Password    string    `json:"-"`
+	CreatedAt   time.Time `json:"createdAT"`
+	HouseholdID int       `json:"householdID"`
 }
 
 //Interfaces are super easy to test so thats why
@@ -24,4 +27,42 @@ type UserStore interface {
 	GetUserByEmail(email string) (*User, error)
 	GetUserByID(id int) (*User, error)
 	CreateUser(User) error
+}
+
+// -- Household types
+type Household struct {
+	ID   int    `json:"ID"`
+	Name string `json:"name"`
+}
+
+type HouseholdStore interface {
+	GetHouseholdByID(id int) (*Household, error)
+	CreateHousehold(Household) error
+}
+
+type RegisterHouseholdPayload struct {
+	Name string `json:"name" validate:"required"`
+}
+
+// -- Chore types
+type Chore struct {
+	ID              int    `json:"ID"`
+	Name            string `json:"name"`
+	XP              int    `json:"xp"`
+	Difficulty      int    `json:"difficulty"`
+	TimeEstimate    int    `json:"timeEstimate"`
+	CompletitonTime int    `json:"completionTime"`
+}
+
+type ChoreStore interface {
+	GetChoreByID(id int) (*Chore, error)
+	CreateChore(Chore) error
+}
+
+type RegisterChorePayload struct {
+	Name            string `json:"name" validate:"required"`
+	XP              int    `json:"xp" validate:"required"`
+	Difficulty      int    `json:"difficulty" validate:"required"`
+	TimeEstimate    int    `json:"timeEstimate" validate:"required"`
+	CompletitonTime int    `json:"completionTime" validate:"required"`
 }
