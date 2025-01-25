fe73baf1bc63d01a32ae8a1ade7b4d90475f8b45
Tim.roma <Timroma6@gmail.com>
2025-01-25 16:20:20 -0500

    Houshold is now completed its get methods also works

--- backend/service/household/routes.go
+++ backend/service/household/routes.go
@@ -3,6 +3,7 @@ package household
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Timber868/roomieranks/types"
 	"github.com/Timber868/roomieranks/utils"
@@ -22,6 +23,7 @@ func NewHandler(store types.HouseholdStore) *Handler {
 // Registers the routes for the api so it handles it when its called
 func (h *Handler) RegisterRoute(router *mux.Router) {
 	router.HandleFunc("/household", h.handleCreateHousehold).Methods("POST")
+	router.HandleFunc("/household/{id}", h.handleGetHousehold).Methods("GET")
 }
 
 func (h *Handler) handleCreateHousehold(w http.ResponseWriter, r *http.Request) {
@@ -52,4 +54,43 @@ func (h *Handler) handleCreateHousehold(w http.ResponseWriter, r *http.Request) 
 
 	// If everything went well we can return a message
 	utils.WriteJSON(w, http.StatusCreated, message)
+}
+
+func (h *Handler) handleGetHousehold(w http.ResponseWriter, r *http.Request) {
+	//Get the id from the url
+	vars := mux.Vars(r)
+	str, ok := vars["id"]
+
+	fmt.Println("ID", str)
+
+	//Validate that the id is there
+	if !ok {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("missing id"))
+		return
+	}
+
+	//Parse the id to an int
+	householdID, err := strconv.Atoi(str)
+
+	//If we cant parse the id we return an error as it should be a number
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	//Get the household from the store
+	household, err := h.store.GetHouseholdByID(householdID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	//If we didnt find the household we return a 404
+	if household == nil {
+		utils.WriteError(w, http.StatusNotFound, fmt.Errorf("household with id %d not found", householdID))
+		return
+	}
+
+	//If we found the household we return it
+	utils.WriteJSON(w, http.StatusOK, household)
 }
